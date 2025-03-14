51750835e50199a127da0a319f1e285087bb179e
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2025-03-14 15:07:10 +0100

    Implement function to get computer choice

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+function getComputerChoice() {
+    computerChoice = "Rock"
+    randNum = Math.floor(Math.random()*3);
+    console.log(randNum)
+
+    switch(randNum){
+        case 0:
+            computerChoice = "Rock";
+            break;
+        case 1:
+            computerChoice = "Paper";
+            break;
+        case 2:
+            computerChoice = "Scissors";
+            break;
+    }
+
+    return computerChoice
+}
+
+computerChoice = getComputerChoice();
+console.log(computerChoice)
