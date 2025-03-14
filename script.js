11399b551f1ce0fb7406d97e64d84d1bdb820a44
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2025-03-14 17:11:25 +0100

    Implement the full game
    Add multiple rounds (5). At the end of the 5 rounds, the scores are compared
    and the winner is returned in the console.

--- script.js
+++ script.js
@@ -1,50 +1,74 @@
-computerScore = 0;
-humanScore = 0;
+playGame();
 
-humanChoice = getHumanChoice()
-computerChoice = getComputerChoice()
-playRound(humanChoice, computerChoice)
+function playGame() {
+    computerScore = 0;
+    humanScore = 0;
 
-function playRound(humanChoice, computerChoice) {
-    humanChoiceLow = humanChoice.toLowerCase();
-    computerChoiceLow = computerChoice.toLowerCase();
-
-    if(humanChoice === computerChoice){
-        console.log("Nobody wins. Try again !")
+    for(let i=0; i<5; i++){
+        humanChoice = getHumanChoice()
+        console.log(humanChoice)
+        computerChoice = getComputerChoice()
+        console.log(computerChoice)
+        playRound(humanChoice, computerChoice)
     }
 
-    switch(humanChoiceLow){
-        case "Rock":
-            switch(computerChoiceLow){
-                case "Paper":
-                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
-                    break;
-                case "Scissors":
-                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
-                    break;
-            }
-            break;
-        case "Paper":
-            switch(computerChoiceLow){
-                case "Rock":
-                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
-                    break;
-                case "Scissors":
-                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
-                    break;
-            }
-            break;
-        case "Scissors":
-            switch(computerChoiceLow){
-                case "Rock":
-                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
-                    break;
-                case "Paper":
-                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
-                    break;
-            }
-            break;
+    if(humanScore > computerScore){
+        console.log("Game is over. Congratulations human. You won that game but never forget that the cake is a lie.")
+    } else if(computerScore > humanScore){
+        console.log("Game is over. I defeated you miserable human. This is just the beginning...")
+    } else {
+        console.log("Nobody won, it's sad.")
     }
+
+    function playRound(humanChoice, computerChoice) {
+        humanChoiceLow = humanChoice.toLowerCase();
+        computerChoiceLow = computerChoice.toLowerCase();
+    
+        if(humanChoiceLow === computerChoiceLow){
+            console.log("Nobody wins. Try again !");
+            return;
+        }
+    
+        switch(humanChoiceLow){
+            case "rock":
+                switch(computerChoiceLow){
+                    case "paper":
+                        console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                        computerScore++;
+                        break;
+                    case "scissors":
+                        console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                        humanScore++;
+                        break;
+                }
+                break;
+            case "paper":
+                switch(computerChoiceLow){
+                    case "rock":
+                        console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                        humanScore++;
+                        break;
+                    case "scissors":
+                        console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                        computerScore++;
+                        break;
+                }
+                break;
+            case "scissors":
+                switch(computerChoiceLow){
+                    case "rock":
+                        console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                        computerScore++;
+                        break;
+                    case "paper":
+                        console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                        humanScore++;
+                        break;
+                }
+                break;
+        }
+    }
+
 }
 
 function getComputerChoice() {
