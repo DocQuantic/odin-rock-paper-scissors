72f9529002a6d9caae9c428ee0ca98d9d3f4dc48
William Magrini <35266773+DocQuantic@users.noreply.github.com>
2025-03-14 16:30:40 +0100

    Add game logic to play one round
    The choices are lower caser and then compared to determine the winner of the round.

--- script.js
+++ script.js
@@ -1,3 +1,52 @@
+computerScore = 0;
+humanScore = 0;
+
+humanChoice = getHumanChoice()
+computerChoice = getComputerChoice()
+playRound(humanChoice, computerChoice)
+
+function playRound(humanChoice, computerChoice) {
+    humanChoiceLow = humanChoice.toLowerCase();
+    computerChoiceLow = computerChoice.toLowerCase();
+
+    if(humanChoice === computerChoice){
+        console.log("Nobody wins. Try again !")
+    }
+
+    switch(humanChoiceLow){
+        case "Rock":
+            switch(computerChoiceLow){
+                case "Paper":
+                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                    break;
+                case "Scissors":
+                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                    break;
+            }
+            break;
+        case "Paper":
+            switch(computerChoiceLow){
+                case "Rock":
+                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                    break;
+                case "Scissors":
+                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                    break;
+            }
+            break;
+        case "Scissors":
+            switch(computerChoiceLow){
+                case "Rock":
+                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                    break;
+                case "Paper":
+                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                    break;
+            }
+            break;
+    }
+}
+
 function getComputerChoice() {
     computerChoice = "Rock"
     randNum = Math.floor(Math.random()*3);
@@ -23,5 +72,4 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-humanChoice = getHumanChoice()
-console.log(humanChoice)
+
