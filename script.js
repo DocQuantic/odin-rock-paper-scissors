9976d51a5b9f538368c5d9b7b2d9a4e1a8cdd69c
DocQuantic <35266773+DocQuantic@users.noreply.github.com>
2025-03-16 14:47:56 +0100

    Add buttons to play the game
    Three buttons are on the screen. Each button corresponds to a choice
    to play the game.
    When a button is pressed, the human choice is set accordingly, the
    computer choice is determined randomly, choices are compared and the
    the scores are updated.

--- script.js
+++ script.js
@@ -1,74 +1,89 @@
-playGame();
+computerScore = 0;
+humanScore = 0;
 
-function playGame() {
-    computerScore = 0;
-    humanScore = 0;
+/*
+humanChoice = getHumanChoice()
+console.log(humanChoice)
+computerChoice = getComputerChoice()
+console.log(computerChoice)
+playRound(humanChoice, computerChoice)
 
-    for(let i=0; i<5; i++){
-        humanChoice = getHumanChoice()
-        console.log(humanChoice)
-        computerChoice = getComputerChoice()
-        console.log(computerChoice)
-        playRound(humanChoice, computerChoice)
-    }
+if(humanScore > computerScore){
+    console.log("Game is over. Congratulations human. You won that game but never forget that the cake is a lie.")
+} else if(computerScore > humanScore){
+    console.log("Game is over. I defeated you miserable human. This is just the beginning...")
+} else {
+    console.log("Nobody won, it's sad.")
+}
+    */
 
-    if(humanScore > computerScore){
-        console.log("Game is over. Congratulations human. You won that game but never forget that the cake is a lie.")
-    } else if(computerScore > humanScore){
-        console.log("Game is over. I defeated you miserable human. This is just the beginning...")
-    } else {
-        console.log("Nobody won, it's sad.")
-    }
-
-    function playRound(humanChoice, computerChoice) {
-        humanChoiceLow = humanChoice.toLowerCase();
-        computerChoiceLow = computerChoice.toLowerCase();
-    
-        if(humanChoiceLow === computerChoiceLow){
-            console.log("Nobody wins. Try again !");
+container = document.querySelector(".container");
+container.addEventListener("click", (e) => {
+    switch(e.target.id){
+        case "rock":
+            humanChoice = "Rock";
+            break;
+        case "paper":
+            humanChoice = "Paper";
+            break;
+        case "scissors":
+            humanChoice = "Scissors";
+            break;
+        default:
             return;
-        }
+    }
     
-        switch(humanChoiceLow){
-            case "rock":
-                switch(computerChoiceLow){
-                    case "paper":
-                        console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
-                        computerScore++;
-                        break;
-                    case "scissors":
-                        console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
-                        humanScore++;
-                        break;
-                }
-                break;
-            case "paper":
-                switch(computerChoiceLow){
-                    case "rock":
-                        console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
-                        humanScore++;
-                        break;
-                    case "scissors":
-                        console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
-                        computerScore++;
-                        break;
-                }
-                break;
-            case "scissors":
-                switch(computerChoiceLow){
-                    case "rock":
-                        console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
-                        computerScore++;
-                        break;
-                    case "paper":
-                        console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
-                        humanScore++;
-                        break;
-                }
-                break;
-        }
+    computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+})
+
+function playRound(humanChoice, computerChoice) {
+    humanChoiceLow = humanChoice.toLowerCase();
+    computerChoiceLow = computerChoice.toLowerCase();
+
+    if(humanChoiceLow === computerChoiceLow){
+        console.log("Nobody wins. Try again !");
+        return;
     }
 
+    switch(humanChoiceLow){
+        case "rock":
+            switch(computerChoiceLow){
+                case "paper":
+                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                    computerScore++;
+                    break;
+                case "scissors":
+                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                    humanScore++;
+                    break;
+            }
+            break;
+        case "paper":
+            switch(computerChoiceLow){
+                case "rock":
+                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                    humanScore++;
+                    break;
+                case "scissors":
+                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                    computerScore++;
+                    break;
+            }
+            break;
+        case "scissors":
+            switch(computerChoiceLow){
+                case "rock":
+                    console.log(`${computerChoice} beats ${humanChoice}. Player loses! The machine defeated you poor little human`)
+                    computerScore++;
+                    break;
+                case "paper":
+                    console.log(`${humanChoice} beats ${computerChoice}. Player wins! You were lucky this time`)
+                    humanScore++;
+                    break;
+            }
+            break;
+    }
 }
 
 function getComputerChoice() {
